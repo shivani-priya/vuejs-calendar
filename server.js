9993b2ca650038714f002bda383bf0a51357d16a
Priya Shivani (IFIN DES TCP SW DEV / EE) <Shivani.Priya-EE@infineon.com>
2021-12-08 18:16:02 +0530

    App3 COMPLETED

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ let events = [
 ];
 
 let renderer;
+//FOR PROD
+if (process.env.NODE_ENV === 'production') {
+  let bundle = fs.readFileSync('./dist/node.bundle.js', 'utf8');
+  renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+  app.use('/dist', express.static(path.join(__dirname, 'dist')));
+}
 
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
